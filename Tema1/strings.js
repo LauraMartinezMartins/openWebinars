4b5009588ea3a070b3e7b4e51ba94c2d0e02e968
lauramartinez <153906212+LauraMartinezMartins@users.noreply.github.com>
2024-04-01 15:35:54 +0000

    Strings en javascript

--- Tema1/strings.js
+++ Tema1/strings.js
@@ -2,4 +2,7 @@ const libs = ["React", "Vue"];
 //se imprime el primer elemento
 console.log(libs[0]);
 const l = libs.length;
-console.log(l);
+console.log('
+$(l>1?'There $(l) :"This library"):
+$(libs.join(","))... //junta todos los elemtnso de la array y juntarlos con la coma
+');
